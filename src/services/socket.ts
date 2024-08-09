038e56734302093a10dfc9bf970bb9d1c689773f
Mridul Dhiman <dhimanmridul91@gmail.com>
2024-08-10 03:36:48 +0530

    remote url

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,11 +1,17 @@
 
 import { Server } from "socket.io";
+import { config } from "dotenv";
+config();
 
 class SocketService {
     private _io: Server;
     constructor() {
         console.log("Init socket server");
-        this._io = new Server();
+        this._io = new Server({
+            cors: {
+                origin: process.env.FRONTEND_URL!
+              }
+        });
     }
     get io() {
         return this._io;
@@ -13,18 +19,20 @@ class SocketService {
 
     public initListeners () {
         const io = this._io;
-        io.on("connection", (socket) => {
+        io.on("connect", (socket) => {
             console.log("User connected: ", socket.id);
+
+            
 
             socket.on("event:message",  ({ activeFile, data }: {activeFile: string, data: string}) => {
                 console.log("Active File: ", activeFile);
                 console.log("Data: ", data);
-                socket.emit("event:server-message", { activeFile, data });
+                io.emit("event:server-message", { activeFile, data });
             });
 
             socket.on("event:visible-files", ({ visibleFiles }: { visibleFiles: string[] }) => {
                 console.log("Visible files: ", visibleFiles)
-                socket.emit("event:sync-visible-files", { visibleFiles });
+                io.emit("event:sync-visible-files", { visibleFiles });
             });
 
             socket.on("disconnect", () => {
