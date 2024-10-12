0fc2e311b1b37d4bcb5cd16c1bd8c973d8d557fc
Mridul Dhiman <dhimanmridul91@gmail.com>
2024-10-13 00:14:53 +0530

    migrate kafka from upstash to aiven cloud

--- src/services/kafka/index.ts
+++ src/services/kafka/index.ts
@@ -1,13 +1,18 @@
 import { Kafka, logLevel } from "kafkajs";
 import { config } from "dotenv";
+import fs from "fs";
+import path from "path";
+
 config();
 
 
 let kafka = new Kafka({
     brokers: [process.env.KAFKA_BROKER!],
-    ssl: true,
+    ssl: {
+        ca: [fs.readFileSync(path.resolve("./ca.pem"), "utf-8")]
+    },
     sasl: {
-        mechanism: 'scram-sha-256',
+        mechanism: 'plain',
         username: process.env.KAFKA_USERNAME!, 
         password: process.env.KAFKA_PASSWD!
     },
