3c0db7c5fb58858c989161423e215e9f83eee0e4
Mridul Dhiman <dhimanmridul91@gmail.com>
2025-01-23 20:04:05 +0530

    [feat] added openapi spec

--- src/routes/v1/room.ts
+++ src/routes/v1/room.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
-import { idValidation } from "../../middleware/http/room";
 import {
   fetchAllRooms,
   fetchLatestRoomFilesState,
 } from "../../controllers/http/room";
+import { paramsValidation } from "../../middleware/http/global";
+import { populateShardId } from "../../middleware/http/shard";
 
 const roomRouter = Router();
 
-roomRouter.get("/:id", idValidation, fetchLatestRoomFilesState);
+interface RoomIdParams {
+  id: number;
+}
+
+roomRouter.get("/:id", paramsValidation<RoomIdParams>, populateShardId, fetchLatestRoomFilesState);
 roomRouter.get("/", fetchAllRooms);
 
 export default roomRouter;
