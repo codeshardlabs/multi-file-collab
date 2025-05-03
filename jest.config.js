b46d74579faa8b327529c96c0653908159304de2
Mridul Dhiman <dhimanmridul91@gmail.com>
2025-05-04 00:30:37 +0530

    made some changes

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
